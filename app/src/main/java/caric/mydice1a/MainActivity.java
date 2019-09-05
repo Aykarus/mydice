a9a7f942944a49a88a6959769f36e8e038e10d5e
chestercari <chester_cari@outlook.com>
2019-09-05 10:52:34 -0400

    added second die

--- app/src/main/java/caric/mydice1a/MainActivity.java
+++ app/src/main/java/caric/mydice1a/MainActivity.java
@@ -11,8 +11,11 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
     public static final String DIE_1 = "die1";
+    public static final String DIE_2 = "die2";
     private TextView die1View;
+    private TextView die2View;
     Die die1 = new Die();
+    Die die2 = new Die();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +23,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         die1View = findViewById(R.id.die1);
+        die2View = findViewById(R.id.die2);
 
         if(savedInstanceState != null){
             die1.setNumber(savedInstanceState.getInt(DIE_1));
+            die2.setNumber(savedInstanceState.getInt(DIE_2));
             displayDice();
         }
 
@@ -34,11 +39,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void rollDice(){
         die1.roll();
+        die2.roll();
         displayDice();
     }
 
     private void displayDice() {
         die1View.setText(String.format(Locale.CANADA,"%1d",die1.getNumber()));
+        die2View.setText(String.format(Locale.CANADA,"%1d",die2.getNumber()));
     }
 
     @Override
@@ -46,5 +53,6 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
 
         outState.putInt(DIE_1,die1.getNumber());
+        outState.putInt(DIE_2,die2.getNumber());
     }
 }
